40e0fbc355f411bb034d221d3c8fba3b42988cd9
andimolzer <andi.molzer@gmail.com>
2016-12-20 09:36:40 +0100

    Projektdateien hinzufügen.

---
+++ ArduMower/ArduMower.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduMower.ino
+ Created:	20.12.2016 09:36:39
+ Author:	Andreas.Moelzer
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
